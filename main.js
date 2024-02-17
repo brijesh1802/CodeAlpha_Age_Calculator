10e426a3326fec05beb5906474fae8e361e9fd1f
Brijesh Poojary <brijeshpujari333@gmail.com>
2024-02-17 09:13:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,33 +5,38 @@ var date = document.getElementById('dob');
     date.max = formattedDate;
 
     function calAge() {
-      var getAge = document.getElementById('getage');
-      getAge.textContent = ''; 
+      var date = document.getElementById('dob');
 
-      var dobValue = date.value;
+function calAge() {
+  var getAge = document.getElementById('getage');
+  getAge.textContent = '';
 
-      
-      if (!dobValue) {
-        getAge.textContent = 'Please select your date of birth.'.fontcolor("red");
-        return;
-      }
+  var currentDate = new Date();
+  currentDate.setFullYear(currentDate.getFullYear() - 1);
+  
+  var dobValue = date.value;
 
-      
-      var dobDate = new Date(dobValue);
+  if (!dobValue) {
+    getAge.textContent = 'Please select your date of birth.'.fontcolor("red");
+    return;
+  }
 
-      
-      if (isNaN(dobDate.getTime())) {
-        getAge.textContent = 'Please enter a valid date.'.fontcolor("red");
-        return;
-      }
+  var dobDate = new Date(dobValue);
 
-      
-      var age = currentDate.getFullYear() - dobDate.getFullYear();
+  if (isNaN(dobDate.getTime())) {
+    getAge.textContent = 'Please enter a valid date.'.fontcolor("red");
+    return;
+  }
 
-      
-      if (currentDate.getMonth() < dobDate.getMonth() || 
-          (currentDate.getMonth() === dobDate.getMonth() && currentDate.getDate() < dobDate.getDate())) {
-        age--;
+  var age = currentDate.getFullYear() - dobDate.getFullYear();
+
+  if (currentDate.getMonth() < dobDate.getMonth() || 
+      (currentDate.getMonth() === dobDate.getMonth() && currentDate.getDate() < dobDate.getDate())) {
+    age--;
+  }
+
+  getAge.textContent = 'Your age is: ' + age + ' years';
+}
       }
 
       
